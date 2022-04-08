888afccfa8b723ac3048402088e0dc2263d27a78
victor <victor@gmail.com>
2022-04-08 10:37:31 -0300

    Testando objects

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,6 +1,20 @@
+import br.com.alura.bytebank.model.Endereco
+
 fun main() {
-    testaContasDiferentes()
+
+    val endereco = Endereco()
+
+    imprime(Unit)
+    imprime(endereco)
+    imprime(1.0)
+    imprime(1)
 }
+
+fun imprime(valor: Any): Any{
+    println(valor)
+    return valor
+}
+
 
 
 
--- src/main/kotlin/br/com/alura/bytebank/model/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/model/Conta.kt
@@ -1,14 +1,20 @@
 package br.com.alura.bytebank.model
 
+
 abstract class Conta(var titular: Cliente, val numeroConta: Int) {
 
     var saldo = 0.0
         protected set
 
-    //init {
-    //  utilizado para execução de um trecho de código
-    //  durante a execução do construtor.
-    // }
+    companion object Contador {
+        var total = 0
+            private set
+    }
+
+    init{
+        println("Criando conta")
+        total++
+    }
 
     fun deposita(valor: Double){
         this.saldo += valor
