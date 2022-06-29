df8ae85e81292177b01e6279b7a991005491ac98
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-06-29 14:29:38 -0300

    Added high order in the SistemaInterno

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,6 +1,8 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.model.Autenticavel
 import br.com.alura.bytebank.model.Endereco
+import br.com.alura.bytebank.model.SistemaInterno
 
 fun main() {
 //    val endereco = Endereco(logradouro = "rua vergueiro", numero = 4545)
@@ -15,8 +17,24 @@ fun main() {
     listOf(Endereco(complemento = "casa"),
     Endereco(),
     Endereco(complemento = "apartamento"))
-        .filter { endereco -> endereco.complemento.isNotEmpty() }
-        .let(::println)
+        .filter (predicate = { endereco -> endereco.complemento.isNotEmpty() })
+        .let(block = (::println))
+
+    soma(1, 5, resultado = (::println))
+
+    val autenticavel = object : Autenticavel{
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    SistemaInterno().entra(autenticavel, senha = 1234, autenticado = {
+        println("Realizar pagamento")
+    })
+}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit){
+    println("Soma sendo efetuada")
+    resultado(a + b)
 }
 
 
--- src/main/kotlin/br/com/alura/bytebank/model/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/model/SistemaInterno.kt
@@ -4,9 +4,10 @@ import br.com.alura.bytebank.model.Autenticavel
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: Int){
+    fun entra(admin: Autenticavel, senha: Int, autenticado:() -> Unit = {}){
         if(admin.autentica(senha)){
             println("Bem vindo ao ByteBank, você autenticou com sucesso !")
+            autenticado()
         }
         else{
             println("Erro na autenticação")
